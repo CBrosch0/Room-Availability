b5c70e4d5cf97accf6dce022e6134114918cf681
Calvin Brosch <53014638+CBrosch0@users.noreply.github.com>
2024-10-05 08:22:47 -0400

    Made CSS naming better for search page

--- src/components/pages/Search.tsx
+++ src/components/pages/Search.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './Search.css'
 import Map from '../search/Map.tsx'
+import Room from '../search/Room.tsx'
 
 
 export default function Search() {
@@ -13,9 +14,9 @@ export default function Search() {
             <br />
             <br />
 
-            <div className="wrapper">
-                <div className="box1"><Map w={1000} /></div>
-                <div className="box2">Two</div>
+            <div className="pagebody">
+                <div className="searchAndMap"><Map w={window.innerWidth * .55} /></div>
+                <div className="Results"><Room /></div>
             </div>
         </>
     )
