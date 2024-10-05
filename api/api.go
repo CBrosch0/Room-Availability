43c776c8f8772c3a9083d6623555e897ad8bb04d
Garrett Webb <webbgamers@gmail.com>
2024-10-05 05:56:02 -0400

    basic api poc

--- api/api.go
+++ api/api.go
@@ -6,61 +6,23 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-var db = make(map[string]string)
-
 func setupRouter() *gin.Engine {
 	// Disable Console Color
 	// gin.DisableConsoleColor()
 	r := gin.Default()
 
 	// Ping test
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(http.StatusOK, Room{123, true, 25, true, true, false, true})
+	r.GET("/ping", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, Room{123, "ENG2 105", true, 25, true, true, false, true, true})
 	})
 
 	// Get user value
-	r.GET("/user/:name", func(c *gin.Context) {
-		user := c.Params.ByName("name")
-		value, ok := db[user]
-		if ok {
-			c.JSON(http.StatusOK, gin.H{"user": user, "value": value})
+	r.GET("/search", func(ctx *gin.Context) {
+		var request SearchRequestPayload
+		if err := ctx.ShouldBindJSON(&request); err != nil {
+			ctx.JSON(http.StatusBadRequest, ErrorResponse{"Invalid JSON"})
 		} else {
-			c.JSON(http.StatusOK, gin.H{"user": user, "status": "no value"})
-		}
-	})
-
-	// Authorized group (uses gin.BasicAuth() middleware)
-	// Same than:
-	// authorized := r.Group("/")
-	// authorized.Use(gin.BasicAuth(gin.Credentials{
-	//	  "foo":  "bar",
-	//	  "manu": "123",
-	//}))
-	authorized := r.Group("/", gin.BasicAuth(gin.Accounts{
-		"foo":  "bar", // user:foo password:bar
-		"manu": "123", // user:manu password:123
-	}))
-
-	/* example curl for /admin with basicauth header
-	   Zm9vOmJhcg== is base64("foo:bar")
-
-		curl -X POST \
-	  	http://localhost:8080/admin \
-	  	-H 'authorization: Basic Zm9vOmJhcg==' \
-	  	-H 'content-type: application/json' \
-	  	-d '{"value":"bar"}'
-	*/
-	authorized.POST("admin", func(c *gin.Context) {
-		user := c.MustGet(gin.AuthUserKey).(string)
-
-		// Parse JSON
-		var json struct {
-			Value string `json:"value" binding:"required"`
-		}
-
-		if c.Bind(&json) == nil {
-			db[user] = json.Value
-			c.JSON(http.StatusOK, gin.H{"status": "ok"})
+			ctx.JSON(http.StatusOK, request)
 		}
 	})
 
@@ -68,7 +30,9 @@ func setupRouter() *gin.Engine {
 }
 
 func main() {
+	// Gin setup
 	r := setupRouter()
+
 	// Listen and Server in 0.0.0.0:8080
 	r.Run(":8080")
 }
--- api/schema.go
+++ api/schema.go
@@ -1,11 +1,28 @@
 package main
 
 type Room struct {
-	Id                int
-	Available         bool
-	Seats             int
-	WhiteBoard        bool
-	AudioAccomodation bool
-	VideoRecording    bool
-	OccupiedNow       bool
+	Id                   int    `json:"id"`
+	Name                 string `json:"name"`
+	IsAvailable          bool   `json:"is_available"`
+	Seats                int    `json:"seats"`
+	HasProjector         bool   `json:"has_projector"`
+	HasWhiteboard        bool   `json:"has_whiteboard"`
+	HasAudioAccomodation bool   `json:"has_audio_accomodation"`
+	HasVideoRecording    bool   `json:"has_video_recording"`
+	IsOccupied           bool   `json:"is_occupied"`
+}
+
+type SearchRequestPayload struct {
+	Seats                int    `json:"seats"`
+	HasProjector         bool   `json:"has_projector"`
+	HasWhiteboard        bool   `json:"has_whiteboard"`
+	HasAudioAccomodation bool   `json:"has_audio_accomodation"`
+	HasVideoRecording    bool   `json:"has_video_recording"`
+	ReservationStart     string `json:"reservation_start"`
+	Reservation_End      string `json:"reservation_end"`
+	IsOccupied           bool   `json:"is_occupied"`
+}
+
+type ErrorResponse struct {
+	Err string `json:"err"`
 }
