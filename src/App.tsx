fac9959c03207f2a8bb32366b718cf927d364f97
Calvin Brosch <53014638+CBrosch0@users.noreply.github.com>
2024-10-05 02:07:20 -0400

    Started room file layout and added temp display to app

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Room from './components/search/room'
 
 function App() {
     const [count, setCount] = useState(0)
@@ -28,6 +29,7 @@ function App() {
                 <p>
                     Edit <code>src/App.tsx</code> and save to test HMR
                 </p>
+                <Room />
             </div>
             <p className="read-the-docs">
                 Click on the Vite and React logos to learn more
