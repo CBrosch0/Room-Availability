f321ebb41f148905c36fc747d6252131350000e1
Garrett Webb <webbgamers@gmail.com>
2024-10-05 06:30:07 -0400

    mongodb connection

--- api/api.go
+++ api/api.go
@@ -1,9 +1,18 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+
+	"github.com/joho/godotenv"
 )
 
 func setupRouter() *gin.Engine {
@@ -30,6 +39,36 @@ func setupRouter() *gin.Engine {
 }
 
 func main() {
+	// Load dotenv
+	err := godotenv.Load()
+	if err != nil {
+		panic(err)
+	}
+	uri := os.Getenv("MONGODB_URI")
+
+	// Mongo connection
+	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
+	opts := options.Client().ApplyURI(uri).SetServerAPIOptions(serverAPI)
+
+	// Create a new client and connect to the server
+	client, err := mongo.Connect(context.TODO(), opts)
+	if err != nil {
+		panic(err)
+	}
+
+	defer func() {
+		if err = client.Disconnect(context.TODO()); err != nil {
+			panic(err)
+		}
+	}()
+
+	// Send a ping to confirm a successful connection
+	if err := client.Database("admin").RunCommand(context.TODO(), bson.D{{"ping", 1}}).Err(); err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Pinged your deployment. You successfully connected to MongoDB!")
+
 	// Gin setup
 	r := setupRouter()
 
