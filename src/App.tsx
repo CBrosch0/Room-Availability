cb703574847347bd9e7e1e5d46740a7b06acf8a8
Calvin Brosch <53014638+CBrosch0@users.noreply.github.com>
2024-10-05 04:41:51 -0400

    Basic room layouts from JSON

--- src/App.tsx
+++ src/App.tsx
@@ -29,7 +29,7 @@ function App() {
                 <p>
                     Edit <code>src/App.tsx</code> and save to test HMR
                 </p>
-                <Map w={1000} h={300} />
+                <Map w={1000} /*h={600}*/ />
             </div>
             <p className="read-the-docs">
                 Click on the Vite and React logos to learn more
--- src/components/search/map.tsx
+++ src/components/search/map.tsx
@@ -1,20 +1,23 @@
 import { useState } from 'react'
 import { Stage, Layer, Rect, Circle } from 'react-konva';
 
+import floorplan from '../../assets/floorplans/layout1.json'
+
 type CanvasSize = {
     w: number
-    h: number
+    /*h: number*/
 }
 
 export default function Map(props: CanvasSize) {
     const [width, setWidth] = useState(props.w)
-    const [height, setHeight] = useState(props.h)
+    const [height, setHeight] = useState(props.w * (6 / 10))
 
-    function getWidth(){
+
+    function getWidth() {
         return width;
     }
 
-    function getHeight(){
+    function getHeight() {
         return height;
     }
 
@@ -25,8 +28,10 @@ export default function Map(props: CanvasSize) {
                     <Rect width={getWidth()} height={getHeight()} fill="white" />
                 </Layer>
                 <Layer>
-                    <Rect width={50} height={50} fill="red" />
-                    <Circle x={200} y={200} stroke="black" radius={50} />
+                    <Rect width={floorplan.building[0].width / 100 * getWidth()} height={floorplan.building[0].height / 100 * getHeight()} x={floorplan.building[0].x / 100 * getWidth()} y={floorplan.building[0].y / 100 * getHeight()} fill="#87abe6" />
+                </Layer>
+                <Layer>
+                    <Rect width={floorplan.rooms[0].width / 100 * getWidth()} height={floorplan.rooms[0].height / 100 * getHeight()} x={floorplan.rooms[0].x / 100 * getWidth()} y={floorplan.rooms[0].y / 100 * getHeight()} fill="#2a7a1d" />
                 </Layer>
             </Stage>
         </>
