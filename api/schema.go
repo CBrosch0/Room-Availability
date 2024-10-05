4c11b27023122ca913fc8edb079062a80ef5e158
Garrett Webb <webbgamers@gmail.com>
2024-10-05 08:24:29 -0400

    update schema

--- api/schema.go
+++ api/schema.go
@@ -1,26 +1,55 @@
 package main
 
+type Building struct {
+	Id     string `json:"id"`
+	Width  int    `json:"width"`
+	Height int    `json:"height"`
+	X      int    `json:"x"`
+	Y      int    `json:"y"`
+}
+
 type Room struct {
-	Id                   int    `json:"id"`
+	Id                   string `json:"id"`
 	Name                 string `json:"name"`
-	IsAvailable          bool   `json:"is_available"`
+	IsAvailable          bool   `json:"isAvailable"`
 	Seats                int    `json:"seats"`
-	HasProjector         bool   `json:"has_projector"`
-	HasWhiteboard        bool   `json:"has_whiteboard"`
-	HasAudioAccomodation bool   `json:"has_audio_accomodation"`
-	HasVideoRecording    bool   `json:"has_video_recording"`
-	IsOccupied           bool   `json:"is_occupied"`
+	HasProjector         bool   `json:"hasProjector"`
+	HasWhiteboard        bool   `json:"hasWhiteboard"`
+	HasAudioAccomodation bool   `json:"hasAudioAccomodation"`
+	HasVideoRecording    bool   `json:"hasVideoVecording"`
+	IsOccupied           bool   `json:"isOccupied"`
+	Width                int    `json:"width"`
+	Height               int    `json:"height"`
+	X                    int    `json:"x"`
+	Y                    int    `json:"y"`
 }
 
 type SearchRequestPayload struct {
 	Seats                int    `json:"seats"`
-	HasProjector         bool   `json:"has_projector"`
-	HasWhiteboard        bool   `json:"has_whiteboard"`
-	HasAudioAccomodation bool   `json:"has_audio_accomodation"`
-	HasVideoRecording    bool   `json:"has_video_recording"`
-	ReservationStart     string `json:"reservation_start"`
-	Reservation_End      string `json:"reservation_end"`
-	IsOccupied           bool   `json:"is_occupied"`
+	HasProjector         bool   `json:"hasProjector"`
+	HasWhiteboard        bool   `json:"hasWhiteboard"`
+	HasAudioAccomodation bool   `json:"hasAudioAccomodation"`
+	HasVideoRecording    bool   `json:"hasVideoRecording"`
+	ReservationStart     string `json:"reservationStart"`
+	Reservation_End      string `json:"reservationEnd"`
+	IsOccupied           bool   `json:"isOccupied"`
+}
+
+type SearchResponsePayload struct {
+	Rooms []Room `json:"rooms"`
+}
+
+type LayoutRequestPayload struct {
+	LayoutId string `json:"layoutId"`
+}
+
+type LayoutResponsePayload struct {
+	Buildings []Building `json:"buildings"`
+	Rooms     []Room     `json:"rooms"`
+}
+
+type ActivityRequestPayload struct {
+	RoomId string `json:"roomId"`
 }
 
 type ErrorResponse struct {
