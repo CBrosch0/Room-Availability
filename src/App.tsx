1bbf98857f413c503c12832b750309535c398b6b
Calvin Brosch <53014638+CBrosch0@users.noreply.github.com>
2024-10-05 03:44:14 -0400

    Set up canvas for map using react-konva

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Room from './components/search/room'
+import Map from './components/search/Map'
 
 function App() {
     const [count, setCount] = useState(0)
@@ -29,7 +29,7 @@ function App() {
                 <p>
                     Edit <code>src/App.tsx</code> and save to test HMR
                 </p>
-                <Room />
+                <Map w={1000} h={300} />
             </div>
             <p className="read-the-docs">
                 Click on the Vite and React logos to learn more
